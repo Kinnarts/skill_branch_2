bfca5e0f474ae41ee51171106094a6d8acfc7c37
Anton Zhirkov <an.zhirkov@gmail.com>
2016-11-09 17:49:59 +0300

    task 2B

--- src/index.js
+++ src/index.js
@@ -14,6 +14,26 @@ app.get('/task2A', (req, res) => {
   res.send(sum.toString());
 });
 
+app.get('/fullname', (req, res) => {
+  if (!req.query.fullname || (req.query.fullname.trim().split(/\s+/).length > 3) || !req.query.fullname.match(/^[\u00C0-\u017Fa-zA-Zа-яА-Я\s\']+$/)) {
+  res.send('Invalid fullname');
+  } else {
+    const fullname = req.query.fullname.trim().split(/\s+/);
+    var fio = toTitleCase(fullname.pop());
+    if (fullname.length != 0) {
+      fullname.forEach(function(item){
+        fio += ' ' + item[0].toUpperCase() + '.'
+      });
+    }
+    res.send(fio);
+  }
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
+function toTitleCase(str)
+{
+  return str.replace(/.*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
